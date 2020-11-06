455cc5e9e1f5577ed065abbe68543293a3a0b0f6
William Russ <william.russ@ifpress.com>
2020-11-06 08:16:09 -0500

    update paths

---
+++ scripts/visualization.R
@@ -0,0 +1,29 @@
+
+# load libraries
+library(tidyverse)
+
+# load local functions
+source("functions/make_descriptive_plot.R")
+source("functions/add_graphical_slide.R")
+
+# load data
+protein_bar_descriptive_data <- readxl::read_xlsx("data/protein_bar_descriptive_data.xlsx")
+
+# create the plots
+protein_bar_descriptive_plots <- protein_bar_descriptive_data %>% 
+  gather(key = "attribute", value = "value", -Product) %>% 
+  group_by(Product) %>% 
+  nest() %>% 
+  mutate(plot = map(data, make_descriptive_plot, flip_axes = TRUE, polar = FALSE)) %>% 
+  select(Product, plot) %>% 
+  deframe()
+
+# create the PowerPoint presentation
+presentation <- officer::read_pptx()
+iwalk(protein_bar_descriptive_plots, function(plot, name) {
+  presentation <- presentation %>% 
+    add_graphical_slide(plot, name)
+})
+
+# output/save the presentation
+print(presentation, target = "output/visualization.pptx")
